37965bbc54b45182cf87875c7675edf8eb8e67ec
ttsutchi <tailegog@gmail.com>
2018-06-28 22:10:13 +0900

    [Add] create extension of HKHealthStore and use that

---
+++ HealthKitSample/HKHealthStore+Extension.swift
@@ -0,0 +1,63 @@
+//
+// Created by Taichi Tsuchida on 2018/06/14.
+// Copyright (c) 2018 Taichi Tsuchida. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+
+extension HKHealthStore {
+
+    func askAuthorization() {
+        let types = Set([
+            HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        ])
+
+        self.requestAuthorization(toShare: types, read: types, completion: {success, error in
+            if success {
+                print("Success")
+            }
+            else {
+                print("Error")
+            }
+        })
+    }
+
+
+    func getSteps(startDate: Date, endDate: Date, completion: ((_ steps: Int) -> Void)!) {
+        let type: HKQuantityType? = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+        let predicate: NSPredicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+
+        let query: HKSampleQuery = HKSampleQuery(sampleType: type!, predicate: predicate, limit: 0, sortDescriptors: nil, resultsHandler: { query, results, error in
+            var steps = 0
+            
+            guard let samples = results else {
+                print(error.debugDescription)
+                return
+            }
+            
+            if samples.count > 0 {
+                for sample in samples as! [HKQuantitySample] {
+                    steps += Int(sample.quantity.doubleValue(for: HKUnit.count()))
+                }
+            }
+            
+            completion(steps)
+        })
+
+        self.execute(query)
+    }
+
+
+    func getStepsOfToday(completion: ((_ steps: Int) -> Void)!) {
+        let calender = Calendar(identifier: .japanese)
+        let startDate: Date = calender.startOfDay(for: Date())
+        let endDate: Date = Date(timeIntervalSinceNow: startDate.timeIntervalSinceNow + 86400)
+
+        print(startDate)
+        print(endDate)
+
+        getSteps(startDate: startDate, endDate: endDate, completion: completion)
+    }
+}
--- HealthKitSample/ViewController.swift
+++ HealthKitSample/ViewController.swift
@@ -19,75 +19,29 @@ class ViewController: UIViewController {
     @IBOutlet var stepsLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let types = Set([
-            HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!,
-            HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!,
-            HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
-        ])
-
-        healthStore.requestAuthorization(toShare: types, read: types, completion: {success, error in
-            if success {
-                print("Success")
-            }
-            else {
-                print("Error")
-            }
-        })
         stepsTitleLabel.layer.masksToBounds = true
         stepsTitleLabel.layer.cornerRadius = 30.0
         distanceTitleLabel.layer.masksToBounds = true
         distanceTitleLabel.layer.cornerRadius = 30.0
 
-//        getSteps()
-        getHeight()
-    }
+        healthStore.askAuthorization()
 
-    func getSteps() {
-        let dateFormatter: DateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        let startDate: Date = dateFormatter.date(from: "2018/05/07")!
-        let endDate: Date = dateFormatter.date(from: "2018/05/11")!
-
-        let type: HKQuantityType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
-        let predicate: NSPredicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
-        let sort: [NSSortDescriptor] = [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)]
-
-        let query: HKSampleQuery = HKSampleQuery(sampleType: type, predicate: predicate, limit: 10, sortDescriptors: sort, resultsHandler: { (query, result, error) in
-            print(result?.first ?? 0)
-        })
-
-        print(healthStore.execute(query))
-    }
-    
-    func getHeight() {
-        let type: HKQuantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height)!
-        let sort: [NSSortDescriptor] = [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)]
-        let query: HKSampleQuery = HKSampleQuery(sampleType: type, predicate: nil, limit: 1, sortDescriptors: sort, resultsHandler: { (query, result , error) in
-            if let e = error {
-                print("Error: \(e.localizedDescription)")
-                return
-            }
-            
+        healthStore.getStepsOfToday(completion: {steps in
             DispatchQueue.main.async {
-                guard let r = result else {
-                    return
-                }
-                print(r)
+                self.stepsLabel.text = steps.description
             }
         })
-        
-        print(healthStore.execute(query))
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
